1d537f34994c0ae94d5903bddac737b0ee7ee7b6
Balsa Sarenac <balsa.smi15@gmail.com>
2020-12-02 21:15:26 +0100

    Create Role and User for authentication and authorization

---
+++ src/main/java/upp/team5/literaryassociation/model/Role.java
@@ -0,0 +1,21 @@
+package upp.team5.literaryassociation.model;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+
+@Data @Entity
+public class Role implements GrantedAuthority {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Override
+    public String getAuthority() {
+        return name;
+    }
+}
---
+++ src/main/java/upp/team5/literaryassociation/model/User.java
@@ -0,0 +1,45 @@
+package upp.team5.literaryassociation.model;
+
+import lombok.Data;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data @Entity
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private Set<Role> roles = new HashSet<>();
+
+}
