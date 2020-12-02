7d9fdf24adf683827b6792e0b96d69ac767b8eff
Balsa Sarenac <balsa.smi15@gmail.com>
2020-12-03 00:33:03 +0100

    Create base security configuration class

--- src/main/java/upp/team5/literaryassociation/configuration/WebSecurityConfig.java
+++ src/main/java/upp/team5/literaryassociation/configuration/WebSecurityConfig.java
@@ -1,24 +1,35 @@
 package upp.team5.literaryassociation.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import upp.team5.literaryassociation.security.CustomUserDetailsService;
+import upp.team5.literaryassociation.security.auth.RestAuthenticationEntryPoint;
+import upp.team5.literaryassociation.security.token.TokenAuthenticationFilter;
+import upp.team5.literaryassociation.security.token.TokenUtils;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new CustomUserDetailsService();
-    }
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
+    @Autowired
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+    @Autowired
+    private TokenUtils tokenUtils;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -26,29 +37,44 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(userDetailsService());
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManagerBean();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
+    // Definisemo nacin autentifikacije
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+
+                .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint).and()
+
+                .authorizeRequests()
 //                .antMatchers("/").hasAnyAuthority("ADMIN", "...")
-//                .anyRequest().authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .formLogin().permitAll()
-                .and()
-                .logout().permitAll()
-                .and()
-                .exceptionHandling().accessDeniedPage("/403");
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/register").permitAll()
+                .anyRequest().authenticated().and()
+
+                .cors().and()
+
+                .addFilterBefore(new TokenAuthenticationFilter(tokenUtils, userDetailsService),
+                        BasicAuthenticationFilter.class);
+
+        http.csrf().disable();
+    }
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // TokenAuthenticationFilter ce ignorisati sve ispod navedene putanje
+        web.ignoring().antMatchers(HttpMethod.POST, "/login");
+        web.ignoring().antMatchers(HttpMethod.POST, "/register");
     }
 }
