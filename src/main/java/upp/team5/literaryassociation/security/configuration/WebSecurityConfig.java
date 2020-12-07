45325a3025a5fd1f5f9a89595ba5ed58ee0255e9
Balsa <balsa.smi15@gmail.com>
2020-12-07 08:06:16 +0100

    Move configuration to security package

--- src/main/java/upp/team5/literaryassociation/security/configuration/WebSecurityConfig.java
+++ src/main/java/upp/team5/literaryassociation/security/configuration/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package upp.team5.literaryassociation.configuration;
+package upp.team5.literaryassociation.security.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
