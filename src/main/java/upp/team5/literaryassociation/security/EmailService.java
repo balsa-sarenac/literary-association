c2cf405f596fb6682ad6ddc80cb2d7bc066b1b22
vtanja <51333448+vtanja@users.noreply.github.com>
2020-12-10 23:13:59 +0100

    EmailService and VerifyService added

---
+++ src/main/java/upp/team5/literaryassociation/security/EmailService.java
@@ -0,0 +1,15 @@
+package upp.team5.literaryassociation.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class EmailService implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+        log.info("Email service hit");
+    }
+}
