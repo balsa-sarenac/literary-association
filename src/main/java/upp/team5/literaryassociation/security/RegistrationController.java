02458670111d62a46d8585cf5defaf61e76d9258
vtanja <51333448+vtanja@users.noreply.github.com>
2020-12-10 21:13:36 +0100

    Added endpoint for generation of author registration form

--- src/main/java/upp/team5/literaryassociation/security/RegistrationController.java
+++ src/main/java/upp/team5/literaryassociation/security/RegistrationController.java
@@ -1,20 +1,34 @@
 package upp.team5.literaryassociation.security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.FormService;
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.TaskService;
+import org.camunda.bpm.engine.form.FormField;
+import org.camunda.bpm.engine.form.TaskFormData;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import upp.team5.literaryassociation.exception.UserAlreadyExistsException;
+import upp.team5.literaryassociation.security.dto.FormFieldsDTO;
 import upp.team5.literaryassociation.security.dto.RegistrationDTO;
+
+import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping(produces = "application/json", path = "/auth")
 public class RegistrationController {
+
+    @Autowired
+    private RuntimeService runtimeService;
+    @Autowired
+    private TaskService taskService;
+    @Autowired
+    private FormService formService;
 
     private RegistrationService registrationService;
 
@@ -28,5 +42,17 @@ public class RegistrationController {
         return registrationService.register(registrationDTO);
     }
 
+    @GetMapping(name = "authorRegForm", path="/author-reg-form")
+    public ResponseEntity<FormFieldsDTO> authorRegFrom() {
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("author-reg");
+        Task task = taskService.createTaskQuery().processInstanceId(processInstance.getId()).list().get(0);
+
+        TaskFormData taskFormData = formService.getTaskFormData(task.getId());
+        List<FormField> properties = taskFormData.getFormFields();
+
+        FormFieldsDTO formFieldsDTO = new FormFieldsDTO(processInstance.getId(), task.getId(), properties);
+
+        return new ResponseEntity<>(formFieldsDTO, HttpStatus.OK);
+    }
 }
 
