163395fd5b81573f1ef6a6ea34b56efaee9b0225
vtanja <51333448+vtanja@users.noreply.github.com>
2020-12-17 12:24:31 +0100

    Added controller for forms

---
+++ src/main/java/upp/team5/literaryassociation/form/controller/FormController.java
@@ -0,0 +1,15 @@
+package upp.team5.literaryassociation.form.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@Slf4j
+@RequestMapping(produces = "application/json", path = "/form")
+public class FormController {
+
+
+}
