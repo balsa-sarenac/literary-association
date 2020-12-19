f21e6bec76966d5cc43fca3d56c7cec3b967c17b
vtanja <51333448+vtanja@users.noreply.github.com>
2020-12-19 13:44:41 +0100

    Added endpoint for getting  processId

--- src/main/java/upp/team5/literaryassociation/form/controller/FormController.java
+++ src/main/java/upp/team5/literaryassociation/form/controller/FormController.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import upp.team5.literaryassociation.common.dto.ProcessDTO;
 import upp.team5.literaryassociation.form.service.GenericFormService;
 import upp.team5.literaryassociation.form.dto.FormFieldsDTO;
 import upp.team5.literaryassociation.form.dto.FormSubmissionDTO;
@@ -31,6 +33,12 @@ public class FormController {
     @PostMapping(name = "submitForm", path = "/submit/{processInstanceId}")
     public void submitForm(@RequestBody FormSubmissionDTO formSubmissionDTO, @PathVariable String processInstanceId) {
         genericFormService.submitForm(processInstanceId, formSubmissionDTO);
+    }
+
+    @PreAuthorize("ROLE_PENDING_AUTHOR")
+    @GetMapping(name = "getProcessId", path = "/getProcessId/{userEmail}")
+    public  ResponseEntity<ProcessDTO> getProcessId(@PathVariable String userEmail){
+        return new ResponseEntity<ProcessDTO>(genericFormService.getProcessId(userEmail), HttpStatus.OK);
     }
 }
 
