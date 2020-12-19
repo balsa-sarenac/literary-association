838f69322c4a5cf958fdd4523277753f3c594f1e
vtanja <51333448+vtanja@users.noreply.github.com>
2020-12-19 13:44:24 +0100

    Updated verification service to set business key of process to user email

--- src/main/java/upp/team5/literaryassociation/register/service/VerificationService.java
+++ src/main/java/upp/team5/literaryassociation/register/service/VerificationService.java
@@ -1,8 +1,11 @@
 package upp.team5.literaryassociation.register.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.IdentityService;
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.identity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import upp.team5.literaryassociation.security.repository.UserRepository;
@@ -15,6 +18,10 @@ public class VerificationService implements JavaDelegate {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RuntimeService runtimeService;
+    @Autowired
+    private IdentityService identityService;
 
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
@@ -25,6 +32,13 @@ public class VerificationService implements JavaDelegate {
         var user = userRepository.getUserByEmail(formSubmission.get("email").toString());
         user.setEnabled(true);
         userRepository.save(user);
+
+        delegateExecution.setProcessBusinessKey(user.getEmail());
+//
+//        User verified = identityService.newUser(user.getEmail());
+//        verified.setPassword(user.getPassword());
+//        verified.setEmail(user.getEmail());
+//        runtimeService.setVariable(delegateExecution.getProcessInstanceId(), "verifiedUser", verified);
 
         log.info("Account verified");
     }
