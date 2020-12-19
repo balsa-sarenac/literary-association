f5c34c9a72f4f88a52f01cfe91547a407c0549cf
Balsa <balsa.smi15@gmail.com>
2020-12-19 18:04:37 +0100

    Add repository annotations

--- src/main/java/upp/team5/literaryassociation/security/repository/RoleRepository.java
+++ src/main/java/upp/team5/literaryassociation/security/repository/RoleRepository.java
@@ -1,10 +1,12 @@
 package upp.team5.literaryassociation.security.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import upp.team5.literaryassociation.model.Role;
 
 import java.util.List;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     Role findByName(String name);
