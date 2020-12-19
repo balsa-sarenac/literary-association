f11d3bc0badac5a845ac9c10afd27e9efbaabdd4
Balsa <balsa.smi15@gmail.com>
2020-12-19 20:11:45 +0100

    Add check if user exists

--- src/main/java/upp/team5/literaryassociation/camunda/validator/EmailValidator.java
+++ src/main/java/upp/team5/literaryassociation/camunda/validator/EmailValidator.java
@@ -10,15 +10,20 @@ import upp.team5.literaryassociation.security.repository.UserRepository;
 @Component
 public class EmailValidator implements FormFieldValidator {
 
-    @Autowired
-    private UserRepository userRepository;
+//    @Autowired
+//    private UserRepository userRepository;
 
     @Override
     public boolean validate(Object o, FormFieldValidatorContext formFieldValidatorContext) {
         String email = (String) o;
 
+        if (email.matches("^[^@\\s]+@[^@\\s\\.]+\\.[^@\\.\\s]+$")) {
+            return false;
+        }
+
 //        User user = this.userRepository.getUserByEmail(email);
 //        return user != null;
-        return email.matches("^[^@\\s]+@[^@\\s\\.]+\\.[^@\\.\\s]+$");
+
+        return true;
     }
 }
