6c62a3a2832622f8121b30ff41e1e63cce4daf23
vtanja <51333448+vtanja@users.noreply.github.com>
2020-12-19 13:42:50 +0100

    Added custom form field (File form field))

---
+++ src/main/java/upp/team5/literaryassociation/camunda/CustomConfiguration.java
@@ -0,0 +1,41 @@
+package upp.team5.literaryassociation.camunda;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.camunda.bpm.engine.impl.cfg.ProcessEnginePlugin;
+import org.camunda.bpm.engine.impl.form.type.AbstractFormFieldType;
+import org.camunda.bpm.spring.boot.starter.configuration.Ordering;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Order(Ordering.DEFAULT_ORDER + 1)
+public class CustomConfiguration implements ProcessEnginePlugin {
+
+
+
+    @Override
+    public void postInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
+
+    }
+
+    @Override
+    public void postProcessEngineBuild(ProcessEngine processEngine) {
+
+    }
+
+    @Override
+    public void preInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
+        if (processEngineConfiguration.getCustomFormTypes() == null) {
+            processEngineConfiguration.setCustomFormTypes(new ArrayList<AbstractFormFieldType>());
+        }
+
+        List<AbstractFormFieldType> formTypes = processEngineConfiguration.getCustomFormTypes();
+        formTypes.add(new FileFormFieldType());
+    }
+
+
+}
