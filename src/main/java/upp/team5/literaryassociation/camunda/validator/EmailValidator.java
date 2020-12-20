878ad34cbab8fba645e399c7f94be9ac4505db61
Balsa <balsa.smi15@gmail.com>
2020-12-20 21:22:41 +0100

    Implement checking if email already exists

--- src/main/java/upp/team5/literaryassociation/camunda/validator/EmailValidator.java
+++ src/main/java/upp/team5/literaryassociation/camunda/validator/EmailValidator.java
@@ -1,29 +1,31 @@
 package upp.team5.literaryassociation.camunda.validator;
 
+import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.impl.form.validator.FormFieldValidator;
 import org.camunda.bpm.engine.impl.form.validator.FormFieldValidatorContext;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import upp.team5.literaryassociation.config.SpringContext;
 import upp.team5.literaryassociation.model.User;
 import upp.team5.literaryassociation.security.repository.UserRepository;
 
-@Component
+@Slf4j
 public class EmailValidator implements FormFieldValidator {
 
-//    @Autowired
-//    private UserRepository userRepository;
+    private UserRepository userRepository;
+
+    public EmailValidator() {
+        this.userRepository = SpringContext.getBean(UserRepository.class);
+    }
 
     @Override
     public boolean validate(Object o, FormFieldValidatorContext formFieldValidatorContext) {
         String email = (String) o;
-
-        if (email.matches("^[^@\\s]+@[^@\\s\\.]+\\.[^@\\.\\s]+$")) {
+        log.info("Checking if email is in correct format");
+        if (!email.matches("^[^@\\s]+@[^@\\s\\.]+\\.[^@\\.\\s]+$")) {
             return false;
         }
 
-//        User user = this.userRepository.getUserByEmail(email);
-//        return user != null;
-
-        return true;
+        log.info("Checking if email already exists");
+        User user = this.userRepository.getUserByEmail(email);
+        return user == null;
     }
 }
