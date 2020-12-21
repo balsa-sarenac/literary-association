5dd4c20e34b60c1524632776c4ff719c0499bca0
vtanja <51333448+vtanja@users.noreply.github.com>
2020-12-21 15:26:05 +0100

    Added endpoint for uploading documents

--- src/main/java/upp/team5/literaryassociation/file/controller/FileController.java
+++ src/main/java/upp/team5/literaryassociation/file/controller/FileController.java
@@ -1,9 +1,17 @@
 package upp.team5.literaryassociation.file.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @CrossOrigin
 @RestController
+@Slf4j
+@RequestMapping(produces = "application/json", path = "/file")
 public class FileController {
+    @PostMapping(name="upload", path = "/upload/{processId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public void upload(@RequestParam("files") MultipartFile file, @PathVariable String processId){
+        log.info("Uploading files");
+    }
 }
