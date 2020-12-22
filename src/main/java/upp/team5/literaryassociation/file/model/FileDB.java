44bf126c5a52ec103e6e8ecf0fc837696364a1d4
vtanja <51333448+vtanja@users.noreply.github.com>
2020-12-22 12:29:42 +0100

    Updated FileDB to be Serializable

--- src/main/java/upp/team5/literaryassociation/file/model/FileDB.java
+++ src/main/java/upp/team5/literaryassociation/file/model/FileDB.java
@@ -6,13 +6,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "files")
 @Setter @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class FileDB {
+public class FileDB implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
