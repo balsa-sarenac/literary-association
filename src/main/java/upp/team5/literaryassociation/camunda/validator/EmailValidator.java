f160196d199429543f85dec5abd1a0dff54de9bb
Balsa <balsa.smi15@gmail.com>
2020-12-22 18:31:15 +0100

    Remove duplicate email exists validation

--- src/main/java/upp/team5/literaryassociation/camunda/validator/EmailValidator.java
+++ src/main/java/upp/team5/literaryassociation/camunda/validator/EmailValidator.java
@@ -3,29 +3,14 @@ package upp.team5.literaryassociation.camunda.validator;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.impl.form.validator.FormFieldValidator;
 import org.camunda.bpm.engine.impl.form.validator.FormFieldValidatorContext;
-import upp.team5.literaryassociation.config.SpringContext;
-import upp.team5.literaryassociation.model.User;
-import upp.team5.literaryassociation.security.repository.UserRepository;
 
 @Slf4j
 public class EmailValidator implements FormFieldValidator {
-
-    private UserRepository userRepository;
-
-    public EmailValidator() {
-        this.userRepository = SpringContext.getBean(UserRepository.class);
-    }
 
     @Override
     public boolean validate(Object o, FormFieldValidatorContext formFieldValidatorContext) {
         String email = (String) o;
         log.info("Checking if email is in correct format");
-        if (!email.matches("^[^@\\s]+@[^@\\s\\.]+\\.[^@\\.\\s]+$")) {
-            return false;
-        }
-
-        log.info("Checking if email already exists");
-        User user = this.userRepository.getUserByEmail(email);
-        return user == null;
+        return email.matches("^[^@\\s]+@[^@\\s\\.]+\\.[^@\\.\\s]+$");
     }
 }
