75f63f5c420b89a6cadff8f1da451e31919b62e1
nikolina-sarenac <nina.sarenac@hotmail.com>
2020-12-27 20:20:51 +0100

    Update ChooseEditorService

--- src/main/java/upp/team5/literaryassociation/publishing/service/ChooseEditorService.java
+++ src/main/java/upp/team5/literaryassociation/publishing/service/ChooseEditorService.java
@@ -1,5 +1,24 @@
 package upp.team5.literaryassociation.publishing.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import upp.team5.literaryassociation.security.repository.UserRepository;
 
-public class ChooseEditorService {
+@Service
+@Slf4j
+public class ChooseEditorService implements JavaDelegate {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void execute(DelegateExecution delegateExecution) throws Exception {
+
+    }
+
+    private int getRandomNumber(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
 }
