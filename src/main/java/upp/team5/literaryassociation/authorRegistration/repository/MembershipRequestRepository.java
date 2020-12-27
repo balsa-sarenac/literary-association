f206b7ebeda5211cda16fc5f9e3ca9170ac1daa0
Balsa <balsa.smi15@gmail.com>
2020-12-27 21:01:29 +0100

    Create repository for membership requests

--- src/main/java/upp/team5/literaryassociation/authorRegistration/repository/MembershipRequestRepository.java
+++ src/main/java/upp/team5/literaryassociation/authorRegistration/repository/MembershipRequestRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import upp.team5.literaryassociation.model.MembershipRequest;
 
+import java.util.List;
+
 @Repository
 public interface MembershipRequestRepository extends JpaRepository<MembershipRequest, Long> {
+
+    List<MembershipRequest> findAllByActive(boolean active);
 }
