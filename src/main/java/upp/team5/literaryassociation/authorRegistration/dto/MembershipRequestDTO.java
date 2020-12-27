2a68a082200d1f404e976dfbefe02a16a781222f
Balsa <balsa.smi15@gmail.com>
2020-12-27 20:59:23 +0100

    Create DTOs for membership requests

---
+++ src/main/java/upp/team5/literaryassociation/authorRegistration/dto/MembershipRequestDTO.java
@@ -0,0 +1,9 @@
+package upp.team5.literaryassociation.authorRegistration.dto;
+
+import upp.team5.literaryassociation.common.dto.UserDTO;
+
+public class MembershipRequestDTO {
+    private Long id;
+    private UserDTO userDTO;
+//    private List<FileDTO> files;
+}
