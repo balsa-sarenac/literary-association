1271737488042617b222eafe9c46caca12ca59ac
Balsa <balsa.smi15@gmail.com>
2020-12-28 14:02:51 +0100

    Implement execption handler for not found exceptions

--- src/main/java/upp/team5/literaryassociation/exception/ExceptionResolver.java
+++ src/main/java/upp/team5/literaryassociation/exception/ExceptionResolver.java
@@ -7,11 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.ws.rs.NotFoundException;
+
 @ControllerAdvice
 public class ExceptionResolver {
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<?> notFoundException(Exception exception) {
+    public ResponseEntity<?> userNotFoundException(Exception exception) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.TEXT_PLAIN);
         return new ResponseEntity<>(exception.getMessage(), headers, HttpStatus.NOT_FOUND);
@@ -31,5 +33,11 @@ public class ExceptionResolver {
         return new ResponseEntity<>(exception.getMessage(), headers, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> notFoundException(Exception exception) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        return new ResponseEntity<>(exception.getMessage(), headers, HttpStatus.NOT_FOUND);
+    }
 
 }
