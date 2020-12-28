599d756a2c4429446fad36a1433ba225f95236e7
Balsa <balsa.smi15@gmail.com>
2020-12-28 12:47:09 +0100

    Implement retrieving membership requests

--- src/main/java/upp/team5/literaryassociation/authorRegistration/dto/MembershipRequestDTO.java
+++ src/main/java/upp/team5/literaryassociation/authorRegistration/dto/MembershipRequestDTO.java
@@ -1,7 +1,14 @@
 package upp.team5.literaryassociation.authorRegistration.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import upp.team5.literaryassociation.common.dto.UserDTO;
 
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MembershipRequestDTO {
     private Long id;
     private UserDTO userDTO;
--- src/main/java/upp/team5/literaryassociation/authorRegistration/service/MembershipRequestService.java
+++ src/main/java/upp/team5/literaryassociation/authorRegistration/service/MembershipRequestService.java
@@ -2,20 +2,22 @@ package upp.team5.literaryassociation.authorRegistration.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.IdentityService;
-import org.camunda.bpm.engine.RuntimeService;
-import org.camunda.bpm.engine.identity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+import upp.team5.literaryassociation.authorRegistration.dto.MembershipRequestDTO;
 import upp.team5.literaryassociation.authorRegistration.repository.MembershipRequestRepository;
-import upp.team5.literaryassociation.file.service.FileService;
+import upp.team5.literaryassociation.common.dto.UserDTO;
+import upp.team5.literaryassociation.common.file.service.FileService;
 import upp.team5.literaryassociation.model.FileDB;
 import upp.team5.literaryassociation.model.MembershipRequest;
+import upp.team5.literaryassociation.model.User;
 import upp.team5.literaryassociation.security.repository.UserRepository;
 
+import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -33,7 +35,6 @@ public class MembershipRequestService {
     @Autowired
     private FileService fileService;
 
-
     public void addNewRequest(HashSet<FileDB> files, String processId) {
         Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         String username;
@@ -44,20 +45,41 @@ public class MembershipRequestService {
              username = principal.toString();
         }
 
-        User user = identityService.createUserQuery().userEmail(username).singleResult();
+        org.camunda.bpm.engine.identity.User user = identityService.createUserQuery().userEmail(username).singleResult();
 
         upp.team5.literaryassociation.model.User dbUser = userRepository.getUserByEmail(username);
 
         MembershipRequest membershipRequest = new MembershipRequest();
 
         membershipRequest.setAuthor(dbUser);
-        membershipRequest.setDocuments((Set<FileDB>)files);
         membershipRequest.setFeePaid(false);
+        membershipRequest.setActive(true);
+        for (FileDB file : files) {
+            membershipRequest.getDocuments().add(file);
+        }
 
         membershipRequest = membershipRequestRepository.save(membershipRequest);
 
-        //dbUser.setMembershipRequest(membershipRequest);
+        dbUser.setMembershipRequest(membershipRequest);
+        this.userRepository.save(dbUser);
 
-        //fileService.updateFiles(files, membershipRequest);
+//        fileService.updateFiles(files, membershipRequest);
+    }
+
+    public List<MembershipRequestDTO> getAllRequests() {
+        log.info("Retrieving all requests from database");
+        List<MembershipRequest> requests = this.membershipRequestRepository.findAllByActive(true);
+
+        List<MembershipRequestDTO> requestDTOS = new ArrayList<>();
+        for (MembershipRequest request : requests) {
+            MembershipRequestDTO membershipRequestDTO = new MembershipRequestDTO();
+            User user = request.getAuthor();
+            UserDTO userDTO = new UserDTO(user.getId(), user.getFirstName(), user.getLastName(),
+                    user.getCity(), user.getCountry(), user.getEmail());
+            membershipRequestDTO.setUserDTO(userDTO);
+            requestDTOS.add(membershipRequestDTO);
+        }
+
+        return requestDTOS;
     }
 }
