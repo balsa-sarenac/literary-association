12283ebfb2439e31b300de0cffbe5c943e3959d7
vtanja <51333448+vtanja@users.noreply.github.com>
2020-12-30 10:47:29 +0100

    Added PublishingRequestRepository

--- src/main/java/upp/team5/literaryassociation/publishing/repository/PublishingRequestRepository.java
+++ src/main/java/upp/team5/literaryassociation/publishing/repository/PublishingRequestRepository.java
@@ -2,12 +2,15 @@ package upp.team5.literaryassociation.publishing.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import upp.team5.literaryassociation.model.Book;
 import upp.team5.literaryassociation.model.PublishingRequest;
 import upp.team5.literaryassociation.model.User;
 
 import java.util.HashSet;
+import java.util.List;
 
 @Repository
 public interface PublishingRequestRepository extends JpaRepository<PublishingRequest, Long> {
-    HashSet<PublishingRequest> findPublishingRequestByBookAuthors(User bookAuthor);
+    PublishingRequest findByBook(Book book);
+    HashSet<PublishingRequest> findByBookAuthors(User book_authors);
 }
