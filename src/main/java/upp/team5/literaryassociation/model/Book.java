e677cf050266b532068d2ea2c7affa79f5eb5fcf
vtanja <51333448+vtanja@users.noreply.github.com>
2020-12-30 11:25:01 +0100

    Added getters and setters for Book

--- src/main/java/upp/team5/literaryassociation/model/Book.java
+++ src/main/java/upp/team5/literaryassociation/model/Book.java
@@ -1,10 +1,19 @@
 package upp.team5.literaryassociation.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Set;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
 
     @Id
