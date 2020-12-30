9880a99051d9b113714bc7ec981ef88f268dfdb3
vtanja <51333448+vtanja@users.noreply.github.com>
2020-12-30 10:48:44 +0100

    Added endpoint for getting publishing requests in PublishingController

--- src/main/java/upp/team5/literaryassociation/publishing/controller/PublishingController.java
+++ src/main/java/upp/team5/literaryassociation/publishing/controller/PublishingController.java
@@ -2,22 +2,31 @@ package upp.team5.literaryassociation.publishing.controller;
 
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.FormService;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.TaskService;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import upp.team5.literaryassociation.common.dto.ProcessDTO;
+import upp.team5.literaryassociation.model.Book;
 import upp.team5.literaryassociation.model.PublishingRequest;
+import upp.team5.literaryassociation.publishing.dto.BookDTO;
+import upp.team5.literaryassociation.publishing.dto.PublishingRequestDTO;
 import upp.team5.literaryassociation.publishing.service.PublishingService;
 
 import java.util.HashSet;
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -37,6 +46,7 @@ public class PublishingController {
         this.publishingService = publishingService;
     }
 
+    @PreAuthorize("hasAuthority('ROLE_AUTHOR')")
     @GetMapping(name = "bookPublishing", path="/start-book-publishing")
     public ResponseEntity<ProcessDTO> startBookPublishingProcess() {
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("Process_1iaa6uo");
@@ -45,12 +55,20 @@ public class PublishingController {
         return new ResponseEntity<>(processDTO, HttpStatus.OK);
     }
 
-    @GetMapping(name = "getAuthorRequests", path="/get/author-requests/{authorId}")
-    public ResponseEntity<?> getAuthorRequests(@PathVariable String authorId) throws JsonProcessingException {
-        HashSet<PublishingRequest> requests = publishingService.getRequests(Long.parseLong(authorId));
-        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
-        String json = ow.writeValueAsString(requests);
-        log.info(json);
-        return new ResponseEntity<>(json, HttpStatus.OK);
+    @PreAuthorize("hasAuthority('ROLE_AUTHOR')")
+    @GetMapping(name = "getAuthorRequests", path="/author-requests/{authorId}")
+    public ResponseEntity<HashSet<PublishingRequestDTO>> getAuthorRequests(@PathVariable String authorId) throws JsonProcessingException {
+        ModelMapper modelMapper = new ModelMapper();
+        HashSet<PublishingRequestDTO> retVal = new HashSet<>();
+
+        List<PublishingRequest> requests = publishingService.getRequests(Long.parseLong(authorId));
+        for(PublishingRequest req : requests){
+            PublishingRequestDTO pubReq = modelMapper.map(req, PublishingRequestDTO.class);
+            retVal.add(pubReq);
+        }
+
+
+
+        return new ResponseEntity<HashSet<PublishingRequestDTO>>(retVal, HttpStatus.OK);
     }
 }
