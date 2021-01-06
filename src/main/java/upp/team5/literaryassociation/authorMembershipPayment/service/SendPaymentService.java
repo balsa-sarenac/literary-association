57e6ff0f159e48737b9479b6ea3a055ec50dc71e
vtanja <51333448+vtanja@users.noreply.github.com>
2021-01-06 16:26:26 +0100

    Implemented SendPaymentService

--- src/main/java/upp/team5/literaryassociation/authorMembershipPayment/service/SendPaymentService.java
+++ src/main/java/upp/team5/literaryassociation/authorMembershipPayment/service/SendPaymentService.java
@@ -1,13 +1,28 @@
 package upp.team5.literaryassociation.authorMembershipPayment.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class SendPaymentService implements JavaDelegate {
+
+    @Autowired
+    private RuntimeService runtimeService;
+
     @Override
     public void execute(DelegateExecution execution) throws Exception {
+        log.info("payment done");
 
+        String processId = execution.getProcessInstanceId();
+        MessageCorrelationResult result = runtimeService.createMessageCorrelation("PaymentReceived")
+                .processInstanceId(processId)
+                .correlateWithResult();
+        result.getExecution();
     }
 }
