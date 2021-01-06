37da7c32fcd8f538266808dba291b03687e637ec
vtanja <51333448+vtanja@users.noreply.github.com>
2021-01-06 03:30:47 +0100

    SubmissionTimeout delegate implemented

---
+++ src/main/java/upp/team5/literaryassociation/authorRegistration/delegate/SubmissionTimeout.java
@@ -0,0 +1,48 @@
+package upp.team5.literaryassociation.authorRegistration.delegate;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import upp.team5.literaryassociation.authorRegistration.service.MembershipRequestService;
+import upp.team5.literaryassociation.common.service.EmailService;
+import upp.team5.literaryassociation.model.MembershipRequest;
+import upp.team5.literaryassociation.model.Role;
+import upp.team5.literaryassociation.model.User;
+import upp.team5.literaryassociation.security.repository.UserRepository;
+import upp.team5.literaryassociation.security.service.RoleService;
+
+import java.util.HashSet;
+
+@Service
+public class SubmissionTimeout implements JavaDelegate {
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private RoleService roleService;
+
+    @Autowired
+    private MembershipRequestService membershipRequestService;
+
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        long membershipId = (long) execution.getVariable("membershipRequestId");
+        MembershipRequest membershipRequest = this.membershipRequestService.getMembershipRequest(membershipId);
+        User user = this.userRepository.findByMembershipRequest(membershipRequest);
+
+        user.setRoles(new HashSet<>());
+
+        membershipRequestService.deleteMembershipRequest(membershipRequest);
+        userRepository.delete(user);
+
+        String to = user.getEmail();
+        String subject = "Submission timeout";
+        String body = "I am sorry but your time to upload more documents has expired. Please register again!";
+
+        this.emailService.Send(to, body, subject);
+    }
+}
