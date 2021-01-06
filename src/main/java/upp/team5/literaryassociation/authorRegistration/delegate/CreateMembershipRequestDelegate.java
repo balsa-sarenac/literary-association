9e0050fac0223e3538ead19968197c604009cc8b
Balsa <balsa.smi15@gmail.com>
2021-01-06 18:29:02 +0100

    Move delegate classes to delegate package

--- src/main/java/upp/team5/literaryassociation/authorRegistration/delegate/CreateMembershipRequestDelegate.java
+++ src/main/java/upp/team5/literaryassociation/authorRegistration/delegate/CreateMembershipRequestDelegate.java
@@ -1,19 +1,20 @@
-package upp.team5.literaryassociation.authorRegistration.service;
+package upp.team5.literaryassociation.authorRegistration.delegate;
 
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import upp.team5.literaryassociation.authorRegistration.service.MembershipRequestService;
 
 @Service
 @Slf4j
-public class CreateMembershipRequest implements JavaDelegate {
+public class CreateMembershipRequestDelegate implements JavaDelegate {
 
     private MembershipRequestService membershipRequestService;
 
     @Autowired
-    public CreateMembershipRequest(MembershipRequestService membershipRequestService) {
+    public CreateMembershipRequestDelegate(MembershipRequestService membershipRequestService) {
         this.membershipRequestService = membershipRequestService;
     }
 
