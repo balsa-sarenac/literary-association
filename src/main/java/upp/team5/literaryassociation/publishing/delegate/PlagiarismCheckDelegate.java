354a9693b4215d00dedd740b6277939887a333e0
Balsa <balsa.smi15@gmail.com>
2021-01-18 19:12:42 +0100

    Fix bug where file was not saved to database

--- src/main/java/upp/team5/literaryassociation/publishing/delegate/PlagiarismCheckDelegate.java
+++ src/main/java/upp/team5/literaryassociation/publishing/delegate/PlagiarismCheckDelegate.java
@@ -5,6 +5,7 @@ import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import upp.team5.literaryassociation.common.file.service.FileService;
 import upp.team5.literaryassociation.model.FileDB;
 import upp.team5.literaryassociation.model.PublishingRequest;
 import upp.team5.literaryassociation.publishing.service.PublishingRequestService;
@@ -21,29 +22,26 @@ public class PlagiarismCheckDelegate implements JavaDelegate {
     @Autowired
     private PublishingRequestService publishingRequestService;
 
+    @Autowired
+    private FileService fileService;
+
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
         log.info("Finding publishing request");
         String correlationId = (String) delegateExecution.getVariable("correlationId");
         Long publishingRequestId = (Long) delegateExecution.getVariable("publishing-request-id");
-        PublishingRequest publishingRequest = this.publishingRequestService.getById(publishingRequestId).orElseThrow(() -> {
-            delegateExecution.getProcessEngineServices()
-                    .getRuntimeService()
-                    .createMessageCorrelation("ErrorEvent")
-                    .processInstanceBusinessKey(correlationId)
-                    .correlateWithResult();
-            throw new NotFoundException("Publishing request with given id doesn't exist.");
-        });
+        PublishingRequest publishingRequest = this.publishingRequestService.getById(publishingRequestId)
+                .orElseThrow(() -> new NotFoundException("Publishing request with given id doesn't exist."));
 
         log.info("Loading dummy files");
         loadFilesAndAddThemToRequest(publishingRequest);
+
         log.info("Saving request with new files");
-        publishingRequestService.savePublishingRequest(publishingRequest);
 
         delegateExecution.getProcessEngineServices()
                 .getRuntimeService()
                 .createMessageCorrelation("PlagiarismCheckDone")
-                .processInstanceBusinessKey(correlationId)
+                .processInstanceId(correlationId)
                 .correlateWithResult();
     }
 
@@ -54,12 +52,14 @@ public class PlagiarismCheckDelegate implements JavaDelegate {
         fileDBFirst.setName("mock-file");
         fileDBFirst.setData(mockFileData);
         fileDBFirst.setType("txt");
+        fileDBFirst.setPublishingRequest(publishingRequest);
         FileDB fileDBSecond = new FileDB();
         fileDBSecond.setName("also-mock-file");
         fileDBSecond.setData(alsoMockFileData);
         fileDBSecond.setType("txt");
+        fileDBSecond.setPublishingRequest(publishingRequest);
 
-        publishingRequest.getPotentialPlagiarismSet().add(fileDBFirst);
-        publishingRequest.getPotentialPlagiarismSet().add(fileDBSecond);
+        fileService.saveFile(fileDBFirst);
+        fileService.saveFile(fileDBSecond);
     }
 }
