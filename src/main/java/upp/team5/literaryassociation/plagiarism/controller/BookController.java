425e84488dc87c8b788b5b78f1d9eddeb3b6bdd7
vtanja <51333448+vtanja@users.noreply.github.com>
2021-01-19 11:22:48 +0100

    Added endpoint for getting author books

---
+++ src/main/java/upp/team5/literaryassociation/plagiarism/controller/BookController.java
@@ -0,0 +1,33 @@
+package upp.team5.literaryassociation.plagiarism.controller;
+
+import javassist.NotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import upp.team5.literaryassociation.common.dto.BookDTO;
+import upp.team5.literaryassociation.common.dto.ProcessDTO;
+import upp.team5.literaryassociation.publishing.service.BookService;
+
+import java.util.HashSet;
+
+@CrossOrigin
+@RestController
+@Slf4j
+@RequestMapping( path = "/book")
+public class BookController {
+    private final BookService bookService;
+
+    BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+
+    @GetMapping(name = "getAuthorBooks", path="/get/{authorId}")
+    public ResponseEntity<HashSet<BookDTO>> getAuthorBooks(@PathVariable String authorId) {
+
+        HashSet<BookDTO> books = bookService.getAuthorBooks(authorId);
+
+        return new ResponseEntity<>(books, HttpStatus.OK);
+    }
+}
