b269946a5f291d2ebd7c7407a005845e4986fd4e
vtanja <51333448+vtanja@users.noreply.github.com>
2021-01-19 15:08:02 +0100

    Added endpoint for filing a complaint

--- src/main/java/upp/team5/literaryassociation/plagiarism/controller/PlagiarismController.java
+++ src/main/java/upp/team5/literaryassociation/plagiarism/controller/PlagiarismController.java
@@ -7,10 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import upp.team5.literaryassociation.common.dto.PlagiarismComplaintDTO;
 import upp.team5.literaryassociation.common.dto.ProcessDTO;
 
 @CrossOrigin
@@ -31,5 +29,11 @@ public class PlagiarismController {
         ProcessDTO processDTO = new ProcessDTO(processInstance.getId());
 
         return new ResponseEntity<>(processDTO, HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_AUTHOR')")
+    @PostMapping(name = "fileComplaint", path="/file-a-complaint/{authorId}")
+    public ResponseEntity<> fileComplaint(@RequestBody PlagiarismComplaintDTO plagiarismComplaintDTO) {
+
     }
 }
