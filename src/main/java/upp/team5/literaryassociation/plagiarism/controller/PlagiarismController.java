332022e1d907c6dab233d773099da338e1c79662
vtanja <51333448+vtanja@users.noreply.github.com>
2021-01-19 11:45:11 +0100

    Added plagiarism controller

---
+++ src/main/java/upp/team5/literaryassociation/plagiarism/controller/PlagiarismController.java
@@ -0,0 +1,14 @@
+package upp.team5.literaryassociation.plagiarism.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@Slf4j
+@RequestMapping( path = "/plagiarism")
+public class PlagiarismController {
+    PlagiarismController(){}
+}
