85701f61c45012f1cefb7ebce5317f5cb25fd997
vtanja <51333448+vtanja@users.noreply.github.com>
2021-01-19 11:44:01 +0100

    Added delegate for notifying chief editor about complaint

---
+++ src/main/java/upp/team5/literaryassociation/plagiarism/delegate/NotifyChiefEditorAboutComplaintDelegate.java
@@ -0,0 +1,15 @@
+package upp.team5.literaryassociation.plagiarism.delegate;
+
+import lombok.extern.slf4j.Slf4j;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class NotifyChiefEditorAboutComplaintDelegate implements JavaDelegate{
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+
+    }
+}
