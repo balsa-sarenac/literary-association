1e73b167ef41f505861efffeb4921b311364257a
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-01-24 22:02:37 +0100

    Remove unnecessary commented lines

--- src/main/java/upp/team5/literaryassociation/publishing/service/ChooseEditorService.java
+++ src/main/java/upp/team5/literaryassociation/publishing/service/ChooseEditorService.java
@@ -47,17 +47,10 @@ public class ChooseEditorService implements JavaDelegate {
         }
 
         Role role = roleService.getByName("ROLE_EDITOR");
-        //Role newRole = roleService.getByName("ROLE_CHIEF_EDITOR");
 
         List<User> editors = userService.getUsersByRole(role.getId());
         int rand = getRandomNumber(0, editors.size()-1);
         User chief = editors.get(rand);
-
-        //Set<Role> currentRoles = chief.getRoles();
-        //currentRoles.add(newRole);
-        //chief.setRoles(currentRoles);
-
-        //userService.saveUser(chief);
 
         HashMap<String, Object> bookInfoSubmission;
         bookInfoSubmission = (HashMap<String, Object>) delegateExecution.getVariable("data-fill-book-info");
