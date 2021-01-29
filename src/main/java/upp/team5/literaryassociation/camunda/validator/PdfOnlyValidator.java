cfca046eda4cb0f19635e42e1a543271d239bf3e
Tanja <tanja.vukmirovic8@gmail.com>
2021-01-29 23:16:41 +0100

    Updated validator for uploading only pdfs

--- src/main/java/upp/team5/literaryassociation/camunda/validator/PdfOnlyValidator.java
+++ src/main/java/upp/team5/literaryassociation/camunda/validator/PdfOnlyValidator.java
@@ -2,7 +2,10 @@ package upp.team5.literaryassociation.camunda.validator;
 
 import org.camunda.bpm.engine.impl.form.validator.FormFieldValidator;
 import org.camunda.bpm.engine.impl.form.validator.FormFieldValidatorContext;
+import upp.team5.literaryassociation.model.Book;
 import upp.team5.literaryassociation.model.FileDB;
+import upp.team5.literaryassociation.model.PlagiarismComplaint;
+import upp.team5.literaryassociation.model.PublishingRequest;
 
 import java.util.HashSet;
 
@@ -11,11 +14,16 @@ public class PdfOnlyValidator implements FormFieldValidator {
     public boolean validate(Object submittedValue, FormFieldValidatorContext validatorContext) {
         HashSet<FileDB> files = (HashSet<FileDB>) submittedValue;
 
-        for(FileDB file : files){
-            if(!file.getType().equals("application/pdf")){
-                return false;
+        if(files!=null){
+            for(FileDB file : files){
+
+                if(!file.getType().equals("application/pdf")){
+                    return false;
+                }
+
             }
         }
+
 
         return true;
     }
