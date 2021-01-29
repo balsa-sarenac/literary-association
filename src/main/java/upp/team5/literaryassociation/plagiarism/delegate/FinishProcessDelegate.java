78f1181cff65152069a615f5286a8b7d609766e0
Balsa <balsa.smi15@gmail.com>
2021-01-29 17:11:20 +0100

    Create delegate for ending the process

--- src/main/java/upp/team5/literaryassociation/plagiarism/delegate/FinishProcessDelegate.java
+++ src/main/java/upp/team5/literaryassociation/plagiarism/delegate/FinishProcessDelegate.java
@@ -3,12 +3,32 @@ package upp.team5.literaryassociation.plagiarism.delegate;
 import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import upp.team5.literaryassociation.model.Book;
+import upp.team5.literaryassociation.model.PlagiarismComplaint;
+import upp.team5.literaryassociation.plagiarism.service.PlagiarismComplaintService;
+import upp.team5.literaryassociation.publishing.service.BookService;
 
 @Service @Slf4j
 public class FinishProcessDelegate implements JavaDelegate {
+
+    @Autowired
+    private PlagiarismComplaintService plagiarismComplaintService;
+    @Autowired
+    private BookService bookService;
+
     @Override
     public void execute(DelegateExecution delegateExecution) throws Exception {
+        log.info("Saving committee members decision");
+        Long  requestId = (Long) delegateExecution.getVariable("plagiarism-complaint-id");
+        PlagiarismComplaint complaint = plagiarismComplaintService.getPlagiarismComplaint(requestId);
+
+        if ((boolean) delegateExecution.getVariable("isPlagiarism")) {
+            Book book = complaint.getPlagiarism();
+            book.setPlagiarism(true);
+            bookService.saveBook(book);
+        }
 
     }
 }
