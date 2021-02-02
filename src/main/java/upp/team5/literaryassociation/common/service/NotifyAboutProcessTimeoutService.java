66882adfd08a76fa9b8d28cf6da5acbf9d7595c0
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-02-02 14:40:10 +0100

    Rename status after timeout

--- src/main/java/upp/team5/literaryassociation/common/service/NotifyAboutProcessTimeoutService.java
+++ src/main/java/upp/team5/literaryassociation/common/service/NotifyAboutProcessTimeoutService.java
@@ -55,7 +55,7 @@ public class NotifyAboutProcessTimeoutService implements JavaDelegate {
         else if(execution.getProcessDefinitionId().split(":")[0].equals("book-publishing")){
             Long id = (Long) execution.getVariable("publishing-request-id");
             PublishingRequest publishingRequest = publishingRequestService.getPublishingRequest(id);
-            publishingRequest.setStatus("Process timeout");
+            publishingRequest.setStatus("Time for publishing has expired");
             publishingRequestService.savePublishingRequest(publishingRequest);
         }
         else if(execution.getProcessDefinitionId().split(":")[0].equals("author-reg")){
